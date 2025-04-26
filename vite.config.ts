530e4ced38c2555f65201e52c621f534bb43a1ba
guardiantzb <3301344200@qq.com>
2025-04-27 01:03:26 +0800

    服务器跨域

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    proxy: {
-      '/tzb/api': {  // 匹配前端请求的前缀
-        target: 'http://localhost:7788',  // 后端地址（无需包含/tzb/api）
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/tzb\/api/, '')  // 可选，根据后端路径调整
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd()) as Record<string, string>
+
+  return {
+    plugins: [vue()],
+    server: {
+      proxy: {
+        '/tzb/api': {  // 匹配前端请求的前缀
+          target: env.VITE_API_BASE_URL,  // 动态读取后端地址
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/tzb\/api/, '')  // 根据后端路径调整
+        }
       }
     }
   }
