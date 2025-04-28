54a1e72985df5da02de695c34882c3c5975c6909
guardiantzb <3301344200@qq.com>
2025-04-28 19:23:15 +0800

    cnm

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import App from './App.vue'
 // 完整引入（简单粗暴，体积大）
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-
+import router from './router/index'
 const app = createApp(App)
+app.use(router)
 app.use(ElementPlus)
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ mode }) => {
+
+  return {
+    plugins: [vue()],
+    server: {
+      proxy: {
+        '/tzb/api': {  // 匹配前端请求的前缀
+          target: "http://42.101.42.177:7788/tzb",  // 动态读取后端地址
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/tzb\/api/, '')  // 根据后端路径调整
+        }
+      }
+    }
+  }
 })
