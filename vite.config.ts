8528fea4c7bf9db0cc0c3a9eccd39e6e6135725e
guardiantzb <3301344200@qq.com>
2025-04-30 19:17:10 +0800

    777

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd()) as Record<string, string>
-
+  console.log('VITE_API_BASE_URL =', env.VITE_API_URL)
   return {
     plugins: [vue()],
     server: {
       proxy: {
         '/tzb/api': {  // 匹配前端请求的前缀
-          target: env.VITE_API_BASE_URL,  // 动态读取后端地址
+          target: env.VITE_API_URL,  // 动态读取后端地址
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/tzb\/api/, '')  // 根据后端路径调整
         }
